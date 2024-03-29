86ddc95c0e47f5baa89b640e385490d776a331cc
Pramod shivaji thete <136362959+Pramodthete@users.noreply.github.com>
2024-03-29 14:29:21 +0530

    Add files via upload

---
+++ solutions/program1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+// this is the System namespace we are using predefined functionality 
+
+namespace program1
+{// this is our custom namespaces
+    internal class Program
+    { // default access modifier is internal which only acess in same namespace or same essembly
+        static void Main(string[] args) // this is main starting point of program
+        {
+            //Console os a class and WriteLine is a method of that class
+            // and method take 1 argument and show it on console
+            Console.WriteLine("This is Project 1");
+
+            Console.Write("Hello");
+            Console.Write(", I am a diffrent print statement but in same line\n");
+
+            //we can also pass expression it will execute with BODMAS rule
+            Console.WriteLine("exp. : "+ (3 + 5 * 2));
+
+            // This is a comment which is sigle line comment
+            // comments are only for developers to make code more readable
+
+            /* and this is multiline comment
+             *  we can write more than one lines
+             */
+        }
+    }
+}
+
